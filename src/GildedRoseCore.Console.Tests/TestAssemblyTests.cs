52b8e88a11df207ca319b400f05f8bc22e5885fc
yeungh <yeung.howard360@gmail.com>
2017-01-09 14:10:20 -0800

    Initial commit
    Attempt at GildedRose kata in the .net core framework.
    Credit goes to Bobby Johnson for rewriting the kata using .net core

---
+++ src/GildedRoseCore.Console.Tests/TestAssemblyTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using ConsoleApplication;
+using Xunit;
+
+
+namespace Tests
+{
+    public class TestAssemblyTests
+    {
+        [Fact]
+        public void TestTheTruth() 
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void LegendaryItemsShouldNotDegradeTest()
+        {
+            
+        }
+
+        public void BackstagePassesShouldIncreaseInQualityAccordingToDaysLeftTest()
+        {
+            var app = new Program();
+            new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20};
+            
+        }
+
+        public void AgedBrieIncreasesInQualityAsDaysPassTest()
+        {
+
+        }
+        
+        public void ItemQualityNeverOverFiftyTest()
+        {
+
+        }
+
+        public void OnceSellDatePassesDegradeQualityByDoubleTest()
+        {
+
+        }
+    }
+}
