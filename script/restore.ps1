d01d28e61a6069afccbbda42f025f53514e23aa9
yeungh <yeung.howard360@gmail.com>
2017-01-09 14:13:33 -0800

    Revert "Initial commit"
    This reverts commit 52b8e88a11df207ca319b400f05f8bc22e5885fc.

--- script/restore.ps1
+++
@@ -1,3 +0,0 @@
-Get-ChildItem ./src/ -r -depth 5 |
-    Where-Object { $_.FullName -match 'project\.json$' } |
-    ForEach-Object { dotnet restore $_.DirectoryName }
--- script/run.ps1
+++
@@ -1,9 +0,0 @@
-Push-Location ./src/GildedRoseCore.Console/
-try 
-{
-    dotnet run  | Write-Output
-}
-finally 
-{
-    Pop-Location
-}
