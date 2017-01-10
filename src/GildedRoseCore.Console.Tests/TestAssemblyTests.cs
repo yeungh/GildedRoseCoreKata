c6fde8e0cb13410c14f6546d5bde45beff55bdcc
yeungh <yeung.howard360@gmail.com>
2017-01-09 16:09:13 -0800

    Added more unit test

--- src/GildedRoseCore.Console.Tests/TestAssemblyTests.cs
+++ src/GildedRoseCore.Console.Tests/TestAssemblyTests.cs
@@ -23,12 +23,13 @@ namespace Tests
         {
             var app = new Program();
             Item itemFifteenDays = new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20}; //no change
-            Item itemTenDays = new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 20}; //quality should be 21
-            Item itemFourDays = new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 4, Quality = 20}; //quality should be 22
+            Item itemTenDays = new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 0}; //quality should be 1
+            Item itemFourDays = new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 4, Quality = 0}; //quality should be 2
 
             app.HandleBackstagePasses(itemTenDays);
-            app.HandleBackstagePasses(itemTenDays);
-            Assert.Equal(itemTenDays.Quality, 21);
+            app.HandleBackstagePasses(itemFourDays);
+            Assert.Equal(1, itemTenDays.Quality);
+            Assert.Equal(2, itemFourDays.Quality);
         }
 
         [Fact]
